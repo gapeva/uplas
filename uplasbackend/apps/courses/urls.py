62b08a11f253625469e66846f7131449e8c2cc74
gapeva <gapevadotpro@gmail.com>
2026-02-06 12:31:44 +0300

    Update urls.py

--- uplasbackend/apps/courses/urls.py
+++ uplasbackend/apps/courses/urls.py
@@ -1,14 +1,22 @@
 # courses/urls.py
 from django.urls import path
-from .views import CourseListView, CourseDetailView, LessonContentView, TeamMemberListView
-
+from .views import (
+    CourseListView, 
+    CourseDetailView, 
+    LessonContentView, 
+    TeamMemberListView,
+    EnrollCourseView
+)
 
 app_name = 'courses'
-
 
 urlpatterns = [
     path('courses/', CourseListView.as_view(), name='course-list'),
     path('courses/<slug:slug>/', CourseDetailView.as_view(), name='course-detail'),
+    
+    # Added Enrollment Endpoint
+    path('courses/<int:pk>/enroll/', EnrollCourseView.as_view(), name='course-enroll'),
+
     path('lessons/<int:pk>/content/', LessonContentView.as_view(), name='lesson-content'),
     path('team/', TeamMemberListView.as_view(), name='team-list'),
 ]
